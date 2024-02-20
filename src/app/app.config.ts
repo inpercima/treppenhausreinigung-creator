ce1b466e317d4ca8e1c7f626215471a83394c02d
inpercima <inpercima@gmail.com>
2024-02-20 12:59:18 +0000

    Deploying to gh-pages from @ inpercima/treppenhausreinigung-creator@dbd0d9ef3be1ecde6537311663a93bb92f4847c7 🚀

--- src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { OverlayModule } from '@angular/cdk/overlay';
-import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-
-export const appConfig: ApplicationConfig = {
-  providers: [importProvidersFrom(BrowserModule, OverlayModule), provideHttpClient(), provideAnimations()],
-};
--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  api: './',
-  appname: 'Treppenhausreinigung creator app',
-  defaultRoute: 'dashboard',
-  production: false,
-  theme: 'indigo-pink',
-};
