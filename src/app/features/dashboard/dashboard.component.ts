7bd657cbf863c0f3cd075e24c207cc87c61aa7c3
Marcel Hendrich <inpercima@gmail.com>
2024-05-12 23:18:18 +0200

    feat(): implement first test for pdf creation

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'tc-dashboard',
   standalone: true,
   templateUrl: './dashboard.component.html',
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSnackBarModule,
+    MatTableModule,
+    ReactiveFormsModule,
+  ],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  form!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      filter: ['', Validators.required],
+    });
+  }
+
+  generatePDF() {
+    const element = document.getElementById('canvas');
+    if (!element) {
+      console.error('Element not found');
+      return;
+    }
+
+    html2canvas(element).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4'); // Portrait, millimeters, A4 size
+      let imgWidth = 208;
+      let imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+      pdf.addImage(imgData, 'PNG', 15, 15, imgWidth, imgHeight);
+      pdf.save('converted-document.pdf');
+    });
+  }
 }
