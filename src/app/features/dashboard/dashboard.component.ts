cd8654da0cba60b6f1e16ada1485821d15649d44
Marcel Hendrich <inpercima@gmail.com>
2024-09-24 23:24:57 +0200

    feat(): update form
    * add form with tenants
    * remove html2canvas
    * generate table with simple data
    * generate pdf

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,58 +1,82 @@
-import html2canvas from 'html2canvas';
-import jsPDF from 'jspdf';
-
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatTableModule } from '@angular/material/table';
-
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { addDays, format } from 'date-fns';
+import jsPDF from 'jspdf';
+
+export interface Row {
+  week: string;
+  tenant: string;
+  completed: string;
+}
 
 @Component({
   selector: 'tc-dashboard',
   standalone: true,
   templateUrl: './dashboard.component.html',
-  imports: [
-    MatButtonModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatPaginatorModule,
-    MatSnackBarModule,
-    MatTableModule,
-    ReactiveFormsModule,
-  ],
+  imports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatTableModule, ReactiveFormsModule],
 })
 export class DashboardComponent implements OnInit {
   form!: FormGroup;
+
+  readonly displayedColumns: string[] = ['week', 'tenant', 'completed'];
+  dataSource = new MatTableDataSource<Row>();
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      filter: ['', Validators.required],
+      tenantLeft: [''],
+      tenantMiddle: [''],
+      tenantRight: [''],
     });
+  }
+
+  onSubmit(): void {
+    const startDate = new Date(2025, 0, 6);
+    const endDate = new Date(2025, 11, 31);
+    let emptyRow = false;
+    let weekStartDate = startDate;
+    let weekEndDate = addDays(startDate, 6);
+    const rows: Row[] = [];
+    while (endDate > weekEndDate) {
+      const test = {
+        week: format(weekStartDate, 'dd.MM.yyyy') + ' - ' + format(weekEndDate, 'dd.MM.yyyy'),
+        tenant: emptyRow ? '' : 'Mieter',
+        completed: '',
+      } as Row;
+      rows.push(test);
+      emptyRow = !emptyRow;
+      weekStartDate = addDays(weekEndDate, 1);
+      weekEndDate = addDays(weekStartDate, 6);
+    }
+    this.dataSource = new MatTableDataSource(rows);
   }
 
   generatePDF() {
     const element = document.getElementById('canvas');
     if (!element) {
-      console.error('Element not found');
+      console.error('Element not found!');
       return;
     }
 
-    html2canvas(element).then((canvas) => {
-      const imgData = canvas.toDataURL('image/png');
-      const pdf = new jsPDF('p', 'mm', 'a4'); // Portrait, millimeters, A4 size
-      const imgWidth = 208;
-      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+    const doc = new jsPDF('p', 'mm', 'a4'); // Portrait, millimeters, A4 size
 
-      pdf.addImage(imgData, 'PNG', 15, 15, imgWidth, imgHeight);
-      pdf.save('converted-document.pdf');
+    doc.html(element, {
+      callback: function (doc) {
+        // Save the PDF
+        doc.save('document-html.pdf');
+      },
+      margin: [10, 10, 10, 10],
+      autoPaging: 'text',
+      x: 0,
+      y: 0,
+      width: 190,
+      windowWidth: 675,
     });
   }
 }
