e537b3516292ef81fdc8bc2a5ca231f0f34669bd
Marcel Hendrich <inpercima@gmail.com>
2024-11-23 14:59:36 +0100

    feat(): add time period

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,60 +1,80 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { addDays, format } from 'date-fns';
 import jsPDF from 'jspdf';
-
-export interface Row {
-  week: string;
-  tenant: string;
-  completed: string;
-}
+import { Row } from './interfaces/row.interface';
 
 @Component({
   selector: 'tc-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
-  imports: [MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatTableModule, ReactiveFormsModule],
+  imports: [MatButtonModule, MatCardModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatTableModule, ReactiveFormsModule],
+  providers: [provideNativeDateAdapter()],
 })
-export class DashboardComponent implements OnInit {
-  private formBuilder = inject(FormBuilder);
+export class DashboardComponent {
+  readonly #formBuilder = inject(NonNullableFormBuilder);
 
-  form!: FormGroup;
+  form = this.#formBuilder.group({
+    tenantLeft: this.#formBuilder.control(''),
+    tenantMiddle: this.#formBuilder.control(''),
+    tenantRight: this.#formBuilder.control(''),
+    floor: this.#formBuilder.control(1, [Validators.required]),
+    startDate: this.#formBuilder.control(new Date(2025, 0, 6), [Validators.required]),
+    endDate: this.#formBuilder.control(new Date(2025, 11, 27), [Validators.required]),
+  });
 
   readonly displayedColumns: string[] = ['week', 'tenant', 'completed'];
   dataSource = new MatTableDataSource<Row>();
 
-  ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      tenantLeft: [''],
-      tenantMiddle: [''],
-      tenantRight: [''],
-    });
+  onSubmit(): void {
+    const tenants: string[] = [];
+    if (this.form.value.tenantLeft) {
+      tenants.push(this.form.value.tenantLeft);
+    }
+    if (this.form.value.tenantMiddle) {
+      tenants.push(this.form.value.tenantMiddle);
+    }
+    if (this.form.value.tenantRight) {
+      tenants.push(this.form.value.tenantRight);
+    }
+    const rows: Row[] = this.#defineRows(tenants);
+    this.dataSource = new MatTableDataSource(rows);
   }
 
-  onSubmit(): void {
-    const startDate = new Date(2025, 0, 6);
-    const endDate = new Date(2025, 11, 31);
+  isContent(): boolean {
+    return !!this.dataSource.data.length;
+  }
+
+  #defineRows(tenants: string[]) {
     let emptyRow = false;
-    let weekStartDate = startDate;
-    let weekEndDate = addDays(startDate, 6);
+    let weekStartDate = this.form.value.startDate;
+    let weekEndDate = addDays(weekStartDate!, 6);
     const rows: Row[] = [];
-    while (endDate > weekEndDate) {
-      const test = {
-        week: `${format(weekStartDate, 'dd.MM.yyyy')} - ${format(weekEndDate, 'dd.MM.yyyy')}`,
-        tenant: emptyRow ? '' : 'Mieter',
+
+    let index = 0;
+    while (this.form.value.endDate! > weekEndDate) {
+      const row = {
+        week: `${format(weekStartDate!, 'dd.MM.yyyy')} - ${format(weekEndDate, 'dd.MM.yyyy')}`,
+        tenant: emptyRow ? '' : tenants[index],
         completed: '',
       } as Row;
-      rows.push(test);
+      rows.push(row);
+
+      if (!emptyRow) {
+        index = index < tenants.length - 1 ? ++index : 0;
+      }
       emptyRow = !emptyRow;
       weekStartDate = addDays(weekEndDate, 1);
       weekEndDate = addDays(weekStartDate, 6);
     }
-    this.dataSource = new MatTableDataSource(rows);
+    return rows;
   }
 
   generatePDF() {
