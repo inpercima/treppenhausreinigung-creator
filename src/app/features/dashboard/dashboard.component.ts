11307a77ef33224315c0c1c0bff6022d8eeb66cc
Marcel Hendrich <inpercima@gmail.com>
2024-12-29 23:37:19 +0100

    feat(): generate content directly without pre-printing to page

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -6,17 +6,16 @@ import { DateAdapter } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { addDays, format } from 'date-fns';
 import jsPDF from 'jspdf';
+import autoTable, { RowInput } from 'jspdf-autotable';
 import { dateRangeValidator } from './date-range.validator';
-import { Row } from './interfaces/row.interface';
 
 @Component({
   selector: 'tc-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
-  imports: [MatButtonModule, MatCardModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, MatTableModule, ReactiveFormsModule],
+  imports: [MatButtonModule, MatCardModule, MatDatepickerModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
 })
 export class DashboardComponent {
   readonly #formBuilder = inject(NonNullableFormBuilder);
@@ -41,9 +40,6 @@ export class DashboardComponent {
     };
   }
 
-  readonly displayedColumns: string[] = ['week', 'tenant', 'completed'];
-  dataSource = new MatTableDataSource<Row>();
-
   onSubmit(): void {
     const tenants: string[] = [];
     if (this.form.value.tenantLeft) {
@@ -55,19 +51,15 @@ export class DashboardComponent {
     if (this.form.value.tenantRight) {
       tenants.push(this.form.value.tenantRight);
     }
-    const rows: Row[] = this.#defineRows(tenants);
-    this.dataSource = new MatTableDataSource(rows);
+    const rows = this.#defineRows(tenants);
+    this.#generatePDF(rows);
   }
 
-  isContent(): boolean {
-    return !!this.dataSource.data.length;
-  }
-
-  #defineRows(tenants: string[]) {
+  #defineRows(tenants: string[]): RowInput[] {
     let emptyRow = false;
     let weekStartDate = this.form.value.startDate;
     let weekEndDate = addDays(weekStartDate!, 6);
-    const rows: Row[] = [];
+    const rows: RowInput[] = [];
 
     let index = 0;
     while (this.form.value.endDate! > weekEndDate) {
@@ -75,7 +67,7 @@ export class DashboardComponent {
         week: `${format(weekStartDate!, 'dd.MM.yyyy')} - ${format(weekEndDate, 'dd.MM.yyyy')}`,
         tenant: emptyRow ? '' : tenants[index],
         completed: '',
-      } as Row;
+      } as RowInput;
       rows.push(row);
 
       if (!emptyRow) {
@@ -88,26 +80,28 @@ export class DashboardComponent {
     return rows;
   }
 
-  generatePDF() {
-    const element = document.getElementById('canvas');
-    if (!element) {
-      console.error('Element not found!');
-      return;
-    }
-
+  #generatePDF(rows: RowInput[]): void {
     const doc = new jsPDF(); // Portrait, millimeters, A4 size
 
-    doc.html(element, {
-      callback: (doc) => {
-        // Save the PDF
-        doc.save('treppenreinigung-plan.pdf');
-      },
-      margin: [0, 10, 5, 10],
-      autoPaging: 'text',
-      x: 0,
-      y: 0,
-      width: 190,
-      windowWidth: 675,
+    const floor = this.form.value.floor === '0' ? 'EG' : `${this.form.value.floor}. OG`;
+    const title = `Treppenreinigung ${floor}`;
+    doc.setFontSize(19);
+    doc.setFont('Helvetica', 'normal', 'bold');
+    doc.text(title, 10, 18);
+
+    const img = new Image();
+    img.src = 'treppenhaus.png';
+    const pageSize = doc.internal.pageSize;
+    doc.addImage(img, 'png', pageSize.getWidth() - 78, 5, 68, 28);
+
+    autoTable(doc, {
+      head: [{ week: 'Woche', tenant: 'Mietpartei', completed: 'Erledigt durch/am' }],
+      body: rows,
+      styles: { cellPadding: 0.8, fontSize: 8 },
+      startY: 35,
+      margin: { left: 10, right: 10 },
     });
+
+    doc.save('treppenreinigung-plan.pdf');
   }
 }
